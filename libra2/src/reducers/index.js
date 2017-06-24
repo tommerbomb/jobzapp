055ecfbda1b949d5fb5c7cb19af100caec9b9ebb
Tom Merkle <tmerk1650@gmail.com>
2017-06-24 16:21:37 -0600

    removed old files

--- libra2/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import AuthReducer from './AuthReducer';
-import RegisterReducer from './RegisterReducer';
-
-
-export default combineReducers({
-  auth: AuthReducer,
-  register: RegisterReducer
-});
